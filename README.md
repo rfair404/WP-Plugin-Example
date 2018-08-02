fc8a50865152d6ffd93cf6d5518f1d4d442435a2
Russell Fair <rfair404@gmail.com>
2018-08-01 22:05:01 -0600

    rough in phpmd, phpcs and phpunit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Example of a WordPress Plugin that follows a TDD approach, includes the WordPress coding standards, PHPMD, and uses CI/CD for automated deployments.
+
+Objectives:
+
+* PHP_Codesniffer
+* WordPress Coding Standards
+* PHPMD
+* eslint
+* stylelint
+* PHP_Unit
+* Qunit
+* Continuous Integration (TravisCI)
+* Continuous Deployment (host TBD)
---
+++ phpunit-bootstrap.php
@@ -0,0 +1,43 @@
+<?php
+define( 'WP_CONTENT_DIR', __DIR__ . '/public_html/custom-content/' );
+define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . 'plugins/' );
+define( 'WP_MU_PLUGIN_DIR', WP_CONTENT_DIR . 'mu-plugins/' );
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+    $_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+// Relative path to Core tests directory.
+if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
+    $_tests_dir = '../../../../tests/phpunit';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
+    trigger_error( 'Unable to locate wordpress-tests-lib', E_USER_ERROR );
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+// Require the composer installed files.
+require_once 'vendor/autoload.php';
+
+// Manually Load each plugin to be tested.
+function _manually_load_plugin() {
+    require_once WP_PLUGIN_DIR . '/path/plugin.php';
+//    require_once WP_MU_PLUGIN_DIR . '/mu-plugin.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Set up the entire test directory.
+require_once $_tests_dir . '/includes/bootstrap.php';
+
+
+
+
+
+
+
+
