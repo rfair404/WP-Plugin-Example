d7a4ebd3a29e9dae1e63fee9f778535a32bb20ba
Russell Fair <rfair404@gmail.com>
2018-08-17 14:04:56 -0400

    add if not exists to db setup

--- custom-content/plugins/custom-plugin/bin/install-wp-tests.sh
+++ custom-content/plugins/custom-plugin/bin/install-wp-tests.sh
@@ -144,7 +144,7 @@ install_db() {
 	fi
 
 	# create database
-	mysqladmin create IF NOT EXISTS  $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
 }
 
 install_wp
